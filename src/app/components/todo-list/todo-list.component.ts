75c15f6c231df2dd0371c2a7ded671a309072de9
Yuliia Melnychenko <yuliia_melnychenko@epam.com>
2019-02-06 17:26:29 +0200

    display items

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-todo-list',
+  selector: 'todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
 export class TodoListComponent implements OnInit {
-
+  todos: object[];
   constructor() { }
 
   ngOnInit() {
+    this.todos = [
+      {
+        id: 1,
+        title: 'Finish Angular course',
+        completed: false,
+        editing: false
+      },
+      {
+        id: 2,
+        title: 'Take over the world',
+        completed: false,
+        editing: false
+      },
+      {
+        id: 3,
+        title: 'Fix js fundamentals',
+        completed: false,
+        editing: false
+      }
+    ];
   }
 
 }
